18c33d8aaeaff4f1699dd8ed801cc89a2c3ddf12
anthonyemvocacao <anthonybezerra388@gmail.com>
2026-04-27 10:33:48 -0300

    Add files via upload

--- explicacao_mod_09.py
+++ explicacao_mod_09.py
@@ -1,1 +1,17 @@
-
+def aula_tratamento_erros():
+    print("--- Desafio 1: Divisao ---")
+    try:
+        numerador = int(input("Digite o numerador "))    
+        denominador = int(input("digite o denominador: "))
+        resultado = numerador / denominador
+    except ValueError:
+        print("Erro: Por favor, digite apenas números inteiros.")
+    except ZeroDivisionError:
+        print("Erro: não podes dividir por zero.")
+    except Exception as erro:
+        print(f"Erro inesperado: {erro}")
+    else:
+        print(f"Sucesso! Resultado: {resultado}")
+    finally:
+        print("--- fim da divisão ---")
+        aula_tratamento_erros()
